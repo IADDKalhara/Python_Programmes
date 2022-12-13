c4786a943bb897ab86fdf88af76c84da473d167e
Damitha Kalhara <45117665+IADDKalhara@users.noreply.github.com>
2022-12-14 02:05:12 +0530

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,32 @@
+#####   ADD 10% DISCOUNT TO SALE VALUE   #####
+
+import openpyxl as xl
+from openpyxl import BarChart, Reference
+
+
+def process_workbook(filename):
+
+    # selecting necessary file to work
+    data_file = xl.load_workbook(filename)
+    sheet1 = data_file["Sheet1"]
+
+
+    for row in range(2, sheet1.max_row + 1):
+        cell = sheet1.cell(row, 3)
+        new_price = cell.value * 0.9
+
+        # saving new value in a new cell
+        new_price_cell = sheet1.cell(row, 5)
+        new_price_cell.value = new_price
+
+    # selecting data cells for making a chart
+    values = Reference(sheet1, min_row = 1, max_row = 4, min_col = 4, max_col = 4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet1.add_chart(chart, "e2")
+
+    # saving as a new file
+    data_file.save(filename)
+
+
