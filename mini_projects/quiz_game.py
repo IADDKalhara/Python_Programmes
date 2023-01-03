e489af7eaeca8aed13bec32d19ef920bbe4a7611
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-03 15:23:30 +0530

    final

--- mini_projects/quiz_game.py
+++ mini_projects/quiz_game.py
@@ -1,10 +1,41 @@
+print("============================")
 print("!!!! Welcome to COMQUIZ !!!!")
 print("============================")
 
-# Asking user if he wants to play. If not exit
-playing = input("Do you want to continue? ")
+# In case of user don't want to play or invalid input, exit programme
+playing = input("Do you want to continue(yes/no)? ")
 
-if (playing.lower() != "yes"):
+if (playing.lower() == "no"):
+    print("Ending Programme....")
+    quit()
+elif (playing.lower() != "yes"):
+    print("Invalid input.. Ending programme ...")
     quit()
 
+# when everything ok, continue
 print("Okay!.. Let's begin!")
+score = 0
+
+answer = input("What is CPU stands for? ")
+if (answer.lower() == "central processing unit"):
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What is GPU stands for? ")
+if (answer.lower() == "graphics processing unit"):
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What is RAM stands for? ")
+if (answer.lower() == "random access memory"):
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+percentage = "{:.2f}".format((score / 3) * 100)
+print(f"You got {str(score)} answers correct :) {percentage}%")
