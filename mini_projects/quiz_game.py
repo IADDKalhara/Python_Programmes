c8f5e17d034fede4fa34741275061726986357e9
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-03 15:23:02 +0530

    final

---
+++ mini_projects/quiz_game.py
@@ -0,0 +1,10 @@
+print("!!!! Welcome to COMQUIZ !!!!")
+print("============================")
+
+# Asking user if he wants to play. If not exit
+playing = input("Do you want to continue? ")
+
+if (playing.lower() != "yes"):
+    quit()
+
+print("Okay!.. Let's begin!")
