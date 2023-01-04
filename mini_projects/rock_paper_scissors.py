2398598a6b780d78cd8c48d766a086b7234a9243
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-04 08:57:44 +0530

    first commit

---
+++ mini_projects/rock_paper_scissors.py
@@ -0,0 +1,11 @@
+import random
+
+options = ["rock" "paper" "scissors"]
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input = input("Enter Rock/Paper/Scissors or Q to quit: ").lower()
+
+    if user
