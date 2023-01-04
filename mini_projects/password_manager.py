1efed0263161e49d59cbf7f2d90c6dcfd12aedb0
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-04 21:59:24 +0530

    working without masterpassword

--- mini_projects/password_manager.py
+++ mini_projects/password_manager.py
@@ -11,7 +11,21 @@ def add():
 
     # Open file in append mode
     with open("passwords.txt", "a") as f:
-        f.write(name + "," + password + "\n")
+        f.write(name + "," + fer.encrypt(password.encode()).decode() + "\n")
+
+
+# Get the encrypt key generated
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+# Get master password from user and combine it with encryption key
+master_password = input("Enter Master Password: ")
+key = load_key() + master_password.encode()
+fer = Fernet(key)
 
 
 def view():
@@ -19,11 +33,18 @@ def view():
         # Read file line by line and print
         for line in f.readlines():
             line = line.rstrip()
-            user, pwd = line.split(",")
-            print(f"Username:{user} | Password:{pwd}")
+            user, password = line.split(",")
+            print(f"Username: {user} | Password: {fer.decrypt(password.encode()).decode()}")
 
+# This is a one time use function
+# Generate a encrypt key and put it in a key file
+'''
+def write_key():
+    encrypt_key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(encrypt_key)
+'''
 
-master_password = input("Enter Master Password: ")
 
 while True:
     choice = input("What do you want to do add / view or q to quit)? ").lower()
