60dd7d3a052e0fd7a67d3e5695635bb4145e16d5
Damitha Kalhara <iaddkalhara1@gmail.com>
2023-01-04 12:55:31 +0530

    first commit

---
+++ mini_projects/password_manager.py
@@ -0,0 +1,15 @@
+master_password = input("Enter Master Password: ")
+
+choice = input("What do you want to do add / view or q to quit)? ").lower()
+
+while True:
+    if choice == "q":
+        quit()
+    elif choice == "add":
+        # Add a new entry
+        pass
+    elif choice == "view":
+        # View entries
+        pass
+
+
